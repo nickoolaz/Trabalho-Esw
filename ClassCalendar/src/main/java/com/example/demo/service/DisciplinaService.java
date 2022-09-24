6c4321735aabf08b0054e93cd7d2d93514cad4ff
Dieyson <dieysonjason@gmail.com>
2022-09-24 15:54:29 -0300

    Implementação do metodo findAll disciplina

---
+++ ClassCalendar/src/main/java/com/example/demo/service/DisciplinaService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Disciplina;
+import java.util.List;
+
+public interface DisciplinaService {
+    public List<Disciplina> listaDisciplinas();
+    
+    
+}
