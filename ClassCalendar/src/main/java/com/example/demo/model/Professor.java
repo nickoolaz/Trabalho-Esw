9a31c6d956b1d4910c86af249c66d316fb81c903
nicolas99correia <nicolas04correia@oulook.com>
2022-09-24 13:25:52 -0300

    Criação do model professor

---
+++ ClassCalendar/src/main/java/com/example/demo/model/Professor.java
@@ -0,0 +1,31 @@
+
+package com.example.demo.model;
+
+public class Professor {
+   private Integer idProfessor;
+   private String  cpf;
+   private String  nome;
+   private String  telefone;
+   private String  endereco;
+
+    public Professor() {
+    }
+
+    public Professor(Integer idProfessor, String cpf, String nome, String telefone, String endereco) {
+        this.idProfessor = idProfessor;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+    
+}
+
+
+
+
+
+
+ 
+
+
