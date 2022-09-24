7c1b53a24b8ac2f00aa2b0ec0c68ae209974b4a6
Dieyson <dieysonjason@gmail.com>
2022-09-24 13:29:51 -0300

    Criação do toString model professor

---
+++ ClassCalendar/src/main/java/com/example/demo/model/Disciplina.java
@@ -0,0 +1,39 @@
+package com.example.demo.model;
+
+public class Disciplina {
+    private Integer idDisciplina;
+    private String materia;
+    private Professor professor;
+
+    public Disciplina() {
+    }
+
+    public Disciplina(Integer idDisciplina, String materia/*, Professor professor*/) {
+        this.idDisciplina = idDisciplina;
+        this.materia = materia;
+        this.professor = professor;
+    }
+
+    public Integer getIdDisciplina() {
+        return idDisciplina;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    @Override
+    public String toString() {
+        return "Disciplina{" + "idDisciplina=" + idDisciplina + ", materia=" + materia + ", professor=" + professor + '}';
+    }
+    
+} 
+
